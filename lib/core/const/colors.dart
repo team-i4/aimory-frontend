409d18e05de5cf0475d90f8a40be615cdc4e2d0c
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-01-03 22:34:23 +0900

    [INIT] 테마 지정 컬러 세팅 (#1)

---
+++ lib/core/const/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// 메인 컬러
+const MAIN_LIGHT_YELLOW = Color(0xFFFFFDF4); // 밝은 노란색 (배경)
+const MAIN_YELLOW = Color(0xFFFFE88E);      // 메인 노란색
+const MAIN_DARK_GREY = Color(0xFF5F5C54);   // 메인 다크 그레이
+
+// 서브 컬러
+const BLACK_COLOR = Color(0xFF111111);      // 검정색
+const DARK_GREY_COLOR = Color(0xFF323232);  // 어두운 회색
+const MID_GREY_COLOR = Color(0xFF545454);   // 중간 회색
+const LIGHT_GREY_COLOR = Color(0xFF828282); // 밝은 회색
+const BORDER_GREY_COLOR = Color(0xFFD9D9D9);// 경계선 회색
