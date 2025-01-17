5a5626632a1ea620d0d858b79b32ae1496e59d7c
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-01-17 23:09:32 +0900

    FEAT(#47): 교사 home 공지사항, 앨범 데이터 연결

--- lib/features/home/screens/teacher_home_screen.dart
+++ lib/features/home/screens/teacher_home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:aimory_app/core/const/colors.dart';
 import 'package:flutter/material.dart';
 import '../../../core/screens/tab_screen.dart';
+import '../../notices/models/notice_model.dart';
+import '../../notices/screens/teacher_notice_detail_screen.dart';
 import '../../notices/screens/teacher_notice_list_screen.dart';
 import '../../photos/screens/teacher_album_screen.dart';
 import '../appbar/teacher_home_app_bar.dart';
@@ -11,6 +13,23 @@ class TeacherHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Notice> noticeItems = List.generate(
+      3, // 3개의 샘플 데이터만 표시
+          (index) => Notice(
+        name: '원장 $index',
+        date: '2025.01.0${index + 1}',
+        description:
+        '오늘 우리 채아는 오전 간식을 아주 잘 먹고 나서 활기차게 놀이를 즐기며 시간을 보냈어요.',
+        imageUrl: 'assets/img/notice_img_sample.jpg',
+      ),
+    );
+
+    // 우리반 앨범 샘플 데이터를 정의
+    final List<String> allPhotos = List.generate(
+      9,
+          (index) => 'https://static.rocketpunch.com/images/jibmusil/index/pc-section5-mood1-min.jpg',
+    );
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
@@ -75,26 +94,38 @@ class TeacherHomeScreen extends StatelessWidget {
                             padding: EdgeInsets.zero,
                             shrinkWrap: true,
                             physics: const NeverScrollableScrollPhysics(),
-                            itemCount: 3, // 샘플 데이터 개수
+                            itemCount: noticeItems.length, // 데이터 개수
                             itemBuilder: (context, index) {
-                              return Container(
-                                margin: const EdgeInsets.all(0), // 카드 외부 여백
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  color: Colors.white, // 배경색
-                                ),
-                                child: Column(
-                                  children: [
-                                    ListTile(
-                                      title: Text("공지사항 제목 $index"),
-                                      subtitle: Text("2024.05.26"),
+                              final notice = noticeItems[index];
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          TeacherNoticeDetailScreen(notice: notice),
                                     ),
-                                    Container(
-                                      margin: EdgeInsets.symmetric(horizontal: 10.0),
-                                      height: 1,
-                                      color: BORDER_GREY_COLOR,
-                                    ),
-                                  ],
+                                  );
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.all(0), // 카드 외부 여백
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    color: Colors.white, // 배경색
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      ListTile(
+                                        title: Text(notice.name),
+                                        subtitle: Text(notice.date),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.symmetric(horizontal: 10.0),
+                                        height: 1,
+                                        color: BORDER_GREY_COLOR,
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               );
                             },
@@ -106,6 +137,7 @@ class TeacherHomeScreen extends StatelessWidget {
                   ),
                   // 메인화면 광고 배너 부분
                   Container(
+                    width: double.infinity,
                     child: Image.asset(
                       'assets/img/banner_sample.png', // 로고 이미지 경로
                       height: 90,
@@ -153,18 +185,26 @@ class TeacherHomeScreen extends StatelessWidget {
                           ],
                         ),
                         const SizedBox(height: 5),
+
                         GridView.builder(
                           shrinkWrap: true,
                           physics: const NeverScrollableScrollPhysics(),
                           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                             crossAxisCount: 3,
-                            crossAxisSpacing: 8,
-                            mainAxisSpacing: 8,
+                            crossAxisSpacing: 8.0,
+                            mainAxisSpacing: 8.0,
                           ),
-                          itemCount: 9, // 샘플 이미지 개수
+                          itemCount: allPhotos.length, // 샘플 이미지 개수
                           itemBuilder: (context, index) {
-                            return Container(
-                              color: BORDER_GREY_COLOR, // 샘플 이미지
+                            final photoUrl = allPhotos[index];
+                            return Container(decoration: BoxDecoration(
+                              color: LIGHT_GREY_COLOR,
+                              image: DecorationImage(
+                                image: NetworkImage(photoUrl),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
                             );
                           },
                         ),
