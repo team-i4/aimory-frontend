72e9af3f145be484713edceeb12605ab741d85bc
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-01-31 12:09:12 +0900

    FEAT(#79): 알림장 전체 조회 API service에 추가

--- lib/features/notes/services/note_service.dart
+++ lib/features/notes/services/note_service.dart
@@ -14,4 +14,10 @@ abstract class NoteService {
       @Header("Authorization") String token,
       @Body() NoteModel note, // ✅ 이제 JSON 변환 없이 NoteModel 객체를 바로 사용 가능!
       );
+
+  // 알림장 전체 조회 API
+  @GET("/notes")
+  Future<List<NoteModel>> fetchNotes(
+      @Header("Authorization") String token,
+      );
 }
