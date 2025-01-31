18cb09744cda7f643517c6985ef24eb7171577ee
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-01-31 17:27:39 +0900

    FEAT(#82): 알림장 단일 조회 provider 추가

--- lib/features/notes/provider/note_provider.dart
+++ lib/features/notes/provider/note_provider.dart
@@ -4,19 +4,19 @@ import '../models/note_model.dart';
 import '../services/note_service.dart';
 import '../../../core/util/secure_storage.dart';
 
-// ✅ Dio 인스턴스 제공
+// Dio 인스턴스 제공
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio();
   return dio;
 });
 
-// ✅ NoteService 인스턴스 제공
+// NoteService 인스턴스 제공
 final noteServiceProvider = Provider<NoteService>((ref) {
   final dio = ref.read(dioProvider);
   return NoteService(dio);
 });
 
-// ✅ 알림장 생성 Provider
+// 알림장 생성 Provider
 final noteCreateProvider = FutureProvider.autoDispose
     .family<NoteModel, NoteModel>((ref, note) async {
   final service = ref.read(noteServiceProvider);
@@ -25,10 +25,20 @@ final noteCreateProvider = FutureProvider.autoDispose
   return service.createNote("Bearer $token", note);
 });
 
-// ✅ 알림장 전체 조회 Provider 추가
+// 알림장 전체 조회 Provider
 final noteListProvider = FutureProvider.autoDispose<List<NoteModel>>((ref) async {
   final service = ref.read(noteServiceProvider);
   final token = await SecureStorage.readToken();
   if (token == null) throw Exception("로그인이 필요합니다.");
   return service.fetchNotes("Bearer $token");
 });
+
+// 알림장 단일 조회 Provider
+final noteDetailProvider = FutureProvider.autoDispose.family<NoteModel, int>((ref, noteId) async {
+  final service = ref.read(noteServiceProvider);
+  final token = await SecureStorage.readToken();
+  if (token == null) throw Exception("로그인이 필요합니다.");
+
+  return service.fetchNoteDetail("Bearer $token", noteId);
+});
+
