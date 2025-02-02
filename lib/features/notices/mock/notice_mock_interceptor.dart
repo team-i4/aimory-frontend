3c7a99b5b4a6d613a511309db2f9c4d3cb5b5faa
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-02-02 15:34:23 +0900

    FEAT(#93): mock 데이터 경로 수정

--- lib/features/notices/mock/notice_mock_interceptor.dart
+++ lib/features/notices/mock/notice_mock_interceptor.dart
@@ -3,10 +3,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 
 class NoticeMockInterceptor extends Interceptor {
+  // static List<dynamic>? cacheData; // JSON 캐싱을 위한
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
     if (options.path == "/notices") { // ✅ 공지사항 목록 조회 요청 시 Mock 데이터 반환
-      final jsonString = await rootBundle.loadString('/dummy_data.json');
+      final jsonString = await rootBundle.loadString('assets/mock/notice_dummy_data.json');
       final jsonData = jsonDecode(jsonString);
 
       return handler.resolve(
