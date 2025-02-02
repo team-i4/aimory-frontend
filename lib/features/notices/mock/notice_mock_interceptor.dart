93bc6b9d5065b761dbc436a2190f117faf931b75
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-02-02 16:07:40 +0900

    FEAT(#95): 공지사항 단일 조회 Mock Interceptor 추가

--- lib/features/notices/mock/notice_mock_interceptor.dart
+++ lib/features/notices/mock/notice_mock_interceptor.dart
@@ -3,17 +3,54 @@ import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 
 class NoticeMockInterceptor extends Interceptor {
-  // static List<dynamic>? cacheData; // JSON 캐싱을 위한
+  static List<dynamic>? cachedData; // JSON 캐싱을 위한
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-    if (options.path == "/notices") { // ✅ 공지사항 목록 조회 요청 시 Mock 데이터 반환
+    if (cachedData == null) {
+      // JSON 데이터 한 번만 로드하여 캐싱
       final jsonString = await rootBundle.loadString('assets/mock/notice_dummy_data.json');
-      final jsonData = jsonDecode(jsonString);
+      cachedData = jsonDecode(jsonString);
+    }
+
+
+    if (options.path == "/notices") {
+      // 공지사항 전체 목록 조회
+      return handler.resolve(
+        Response(
+          requestOptions: options,
+          data: cachedData,
+          statusCode: 200,
+        ),
+      );
+    } else if (options.path.startsWith("/notices/")) {
+      //  공지사항 단일 조회
+      final noticeId = int.tryParse(options.path.split("/").last); // ID 추출
+      if (noticeId == null) {
+        return handler.reject(DioException(
+          requestOptions: options,
+          error: "ID가 올바르지 않습니다.",
+          type: DioExceptionType.badResponse,
+        ));
+      }
+
+      final notice = cachedData!.firstWhere(
+            (n) => n["id"] == noticeId,
+        orElse: () => null,
+      );
+
+      if (notice == null) {
+        return handler.reject(DioError(
+          requestOptions: options,
+          error: "공지사항을 찾을 수 없습니다.",
+          type: DioErrorType.badResponse,
+        ));
+      }
 
       return handler.resolve(
         Response(
           requestOptions: options,
-          data: jsonData,
+          data: notice,
           statusCode: 200,
         ),
       );
