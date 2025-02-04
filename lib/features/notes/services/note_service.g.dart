8897d3f51bbd6fb90395c39fbc5ce5945c481813
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-02-05 00:53:41 +0900

    FEAT(#118): 알림장 삭제 기능 추가

--- lib/features/notes/services/note_service.g.dart
+++ lib/features/notes/services/note_service.g.dart
@@ -90,6 +90,27 @@ class _NoteService implements NoteService {
     return _value;
   }
 
+  @override
+  Future<void> deleteNotes(String token, Map<String, dynamic> noteIds) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{r'Authorization': token};
+    _headers.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    _data.addAll(noteIds);
+    final _options = _setStreamType<void>(
+      Options(method: 'DELETE', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/notes',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    await _dio.fetch<void>(_options);
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
