d6861dcf548688f687e98ff19ae75400afc8e3b6
SunnySunny <51068439+sunyoung-park@users.noreply.github.com>
2025-03-02 20:46:25 +0900

    FEAT(#134): 원장 Role 여린이집 정보 등록하기 스크린 화면 생성 및 UI 구현

---
+++ lib/features/director_settings/screens/center_insert_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:developer';
+
+import 'package:aimory_app/core/const/colors.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/widgets/custom_button.dart';
+import '../../../core/widgets/custom_input_decoration.dart';
+
+class CenterInsertScreen extends ConsumerWidget {
+  const CenterInsertScreen({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MAIN_YELLOW,
+        centerTitle: true,
+        title: const Text("어린이집 정보 등록", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600)),
+        leading: IconButton(icon: const Icon(Icons.keyboard_backspace), onPressed: () => Navigator.pop(context)),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
